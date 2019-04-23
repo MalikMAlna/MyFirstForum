c1fa5b7a4cb846db1347c9b1002b85d8326076c1
MaJetiGizzle <jimalik2525@gmail.com>
2019-04-23 19:04:52 -0400

    Add project files.

---
+++ MyFirstForum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

014afa9544232f2614f7913275cb7c9a1f4eadd9
MaJetiGizzle <jimalik2525@gmail.com>
2019-04-24 14:59:29 -0400

    Created Models
    For ORM Entity Framework DB.

--- MyFirstForum.Data/ApplicationDbContext.cs
+++ MyFirstForum.Data/ApplicationDbContext.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using MyFirstForum.Models;
+using MyFirstForum.Data.Models;
 
 namespace MyFirstForum.Data
 {
@@ -15,12 +11,7 @@ namespace MyFirstForum.Data
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
     }
 }

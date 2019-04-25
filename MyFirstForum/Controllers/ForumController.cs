7a1db74408ecbaddf3f06704924fe29a6449ddc3
MaJetiGizzle <jimalik2525@gmail.com>
2019-04-25 14:32:34 -0400

    Added to Web Layer, Added Forum Controller, View, Model for forum listing and index. Also added wrote GetAll()

---
+++ MyFirstForum/Controllers/ForumController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstForum.Data;
+using MyFirstForum.Data.Models;
+using MyFirstForum.Models.Forum;
+
+namespace MyFirstForum.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+                .Select(forum => new ForumListingModel {
+                    Id = forum.Id,
+                    Name = forum.Title,
+                    Description = forum.Description
+            });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+    }
+}

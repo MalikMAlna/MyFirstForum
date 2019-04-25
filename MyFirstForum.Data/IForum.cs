8cee7ff839a552cd5410f3e28cc230c844edd42b
MaJetiGizzle <jimalik2525@gmail.com>
2019-04-25 14:10:21 -0400

    Added Interface to Service Layer, IForum including some basic CRUD operations

---
+++ MyFirstForum.Data/IForum.cs
@@ -0,0 +1,21 @@
+﻿using MyFirstForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstForum.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int Id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+
+    }
+}
--- MyFirstForum.Service/ForumService.cs
+++ MyFirstForum.Service/ForumService.cs
@@ -1,8 +1,55 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstForum.Data;
+using MyFirstForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MyFirstForum.Service
 {
-    public class ForumService
+    public class ForumService : IForum
     {
+        private readonly ApplicationDbContext _context;
+
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

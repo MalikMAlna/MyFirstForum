457196022aed97cf41022310f4e3c50cdbf69961
MaJetiGizzle <jimalik2525@gmail.com>
2019-05-02 16:13:45 -0400

    Added PostReplyModel, IEnum for PostIndexModel

--- MyFirstForum/Models/Post/PostIndexModel.cs
+++ MyFirstForum/Models/Post/PostIndexModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstForum.Models.Reply;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,16 @@ namespace MyFirstForum.Models.Post
 {
     public class PostIndexModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public int AuthorRating { get; set; }
+        public DateTime Created { get; set; }
+        public string PostContent { get; set; }
+        
+        public IEnumerable<PostReplyModel> Replies { get; set; }
+
     }
 }
